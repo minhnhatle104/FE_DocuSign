cbe9e0e3eb3e6225ff3d0da067e71207ca5823ca
minhnhatle104 <lnminh104@gmail.com>
2023-02-28 18:13:40 +0700

    Set Up Structure Folder + MUI Library

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import LoginForm from './components/common/LoginForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginForm/>} />
+    </Routes>
+  </BrowserRouter>
 )
